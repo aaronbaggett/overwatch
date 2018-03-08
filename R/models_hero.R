256fbf32ca65ffab80775ad675c5aea2f0e94840
Aaron Baggett <abaggett@umhb.edu>
2018-03-08 10:40:48 -0600

    Add model coefficients

--- R/models_hero.R
+++ R/models_hero.R
@@ -18,23 +18,27 @@ mdat$hero.role <- stringr::str_to_title(mdat$hero.role)
 # Model without respect to grouping
 m0 <- lm(rating ~ fwin, data = mdat)
 summary(m0)
+coef(m0)
 sem.model.fits(m0)
 mdat$simple_model <- predict(m0)
 
 # Model with varying intercepts
 m1 <- lmer(rating ~ fwin + (1 | hero), data = mdat)
 summary(m1)
+coef(m1)
 sem.model.fits(m1)
 mdat$pred_ri <- predict(m1)
 
 # Model with varying slopes
 m2 <- lmer(rating ~ fwin + (0 + fwin | hero), data = mdat)
 summary(m2)
+coef(m2)
 sem.model.fits(m2)
 mdat$pred_rs <- predict(m2)
 
 # Model with varying slopes and intercepts
 m3 <- lmer(rating ~ fwin + (1 + fwin | hero), data = mdat)
 summary(m3)
+coef(m3)
 sem.model.fits(m3)
 mdat$pred_rsi <- predict(m3)
--- R/models_role.R
+++ R/models_role.R
@@ -18,23 +18,27 @@ mdat$hero.role <- stringr::str_to_title(mdat$hero.role)
 # Model without respect to grouping
 m0.1 <- lm(rating ~ fwin, data = mdat)
 summary(m0.1)
+coef(m0.1)
 sem.model.fits(m0.1)
 mdat$simple_model_role <- predict(m0.1)
 
 # Model with varying intercepts
 m1.1 <- lmer(rating ~ fwin + (1 | hero.role), data = mdat)
 summary(m1.1)
+coef(m1.1)
 sem.model.fits(m1.1)
 mdat$pred_ri_role <- predict(m1.1)
 
 # Model with varying slopes
 m2.1 <- lmer(rating ~ fwin + (0 + fwin | hero.role), data = mdat)
 summary(m2.1)
+coef(m2.1)
 sem.model.fits(m2.1)
 mdat$pred_rs_role <- predict(m2.1)
 
 # Model with varying slopes and intercepts
 m3.1 <- lmer(rating ~ fwin + (1 + fwin | hero.role), data = mdat)
 summary(m3.1)
+coef(m3.1)
 sem.model.fits(m3.1)
 mdat$pred_rsi_role <- predict(m3.1)
