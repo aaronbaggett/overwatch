be8461ce7fabf2fc1f10ed465c0b4c69c878373e
Aaron Baggett <abaggett@umhb.edu>
2018-03-09 16:43:54 -0600

    Update hero colors

--- R/figs_hero.R
+++ R/figs_hero.R
@@ -17,12 +17,12 @@ names(cols) <- c("hero", "color")
 fdat <- inner_join(fdat, cols)
 
 # M0 plot
-ggplot(data = fdat, aes(x = fwin, y = rating, color = hero)) + 
+ggplot(data = fdat, aes(x = fwin, y = rating, color = color)) + 
   geom_point(size = 4, alpha = .5) + 
-  scale_x_continuous(limits = c(0.1, 0.9), 
+    scale_x_continuous(limits = c(0.1, 0.9), 
     breaks = seq(0, 1, 0.1), name = "\nFight Win Rate") +
   scale_y_continuous(breaks = seq(100, 2000, 200), "Skill Rating\n") +
-  geom_line(aes(x = fwin, y = simple.model), color = "gray45", size = 1) + 
+  geom_line(aes(x = fwin, y = simple_model), color = "gray45", size = 1) + 
   scale_color_discrete("Hero") + theme_bw() +
   theme(plot.subtitle = element_text(vjust = 1), 
     plot.caption = element_text(vjust = 1), 
@@ -35,7 +35,7 @@ ggplot(data = fdat, aes(x = fwin, y = rating, color = hero)) +
     legend.position = "bottom") +
   ggtitle("Fixed Effects Null Model") + 
   theme(plot.title = element_text(size = 18, 
-    face = "bold"))
+    face = "bold")) + scale_color_manual(values = fdat$color)
 
 # M1 plot
 ggplot(data = fdat, aes(x = fwin, y = rating, color = hero)) + 
@@ -57,7 +57,7 @@ ggplot(data = fdat, aes(x = fwin, y = rating, color = hero)) +
     legend.position = "bottom") +
   ggtitle("Random Intercepts Model") + 
   theme(plot.title = element_text(size = 18, 
-    face = "bold"))
+    face = "bold")) + scale_color_manual(values = fdat$color)
 
 # M2 plot
 ggplot(data = fdat, aes(x = fwin, y = rating, color = hero)) + 
